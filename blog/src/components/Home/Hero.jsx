1055c56d5840001d4b5c2fe9e0db24aac853d86b
Tamim Fatema <itrade83@gmail.com>
2024-09-26 15:57:39 +0600

    starting the website

--- blog/src/components/Home/Hero.jsx
+++ blog/src/components/Home/Hero.jsx
@@ -4,11 +4,14 @@ import React from "react";
 const Hero = () => {
   return (
     <>
-      <div className="relative">
+       <div className="relative">
         <Image src="/hero.jpg" width={2000} height={200} />
-        <p className="absolute top-0 left-0 text-white text-4xl my-4 mx-8">
+        <p className="absolute top-0 left-0 text-white text-8xl my-4 mx-8 p-32">
           Let's Talk
         </p>
+        <button className="absolute top-0 left-0 border border-white text-white p-2">
+          Read More
+        </button>
       </div>
     </>
   );
